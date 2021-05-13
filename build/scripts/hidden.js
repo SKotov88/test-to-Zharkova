5e15660c7cb44bd71d67a60b1fd8009204390e4e
SKotov88 <kotov.zs@mail.ru>
2021-05-13 21:30:29 +0300

    test
    test

---
+++ build/scripts/hidden.js
@@ -0,0 +1,27 @@
+function readMore() {
+    var btn1 = document.getElementById("btn1");
+    var hidden1 = document.querySelector('.hidden-1');
+    var hidden2 = document.querySelector('.hidden-2');
+    var hidden3 = document.querySelector('.hidden-3');
+    var hidden4 = document.querySelector('.hidden-4');
+
+    hidden1.style.display='flex';
+    hidden2.style.display='flex';
+    hidden3.style.display='flex';
+    hidden4.style.display='flex';
+    btn1.style.display='none';
+}
+
+function readMore1() {
+    var btn2 = document.getElementById("btn2");
+    var hidden5 = document.querySelector('.hidden-5');
+    var hidden6 = document.querySelector('.hidden-6');
+    var hidden7 = document.querySelector('.hidden-7');
+    var hidden8 = document.querySelector('.hidden-8');
+
+    hidden5.style.display='flex';
+    hidden6.style.display='flex';
+    hidden7.style.display='flex';
+    hidden8.style.display='flex';
+    btn2.style.display='none';
+}
